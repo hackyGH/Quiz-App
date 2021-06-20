9c1948783f11d2d2de4df00142cffa11d5b7d31a
Gaurav Habad <gauravhabad113@gmail.com>
2021-06-21 01:27:04 +0530

    Updates

---
+++ precache-manifest.fb9548aba865d8966bcc46dc71749034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737ef501b98a63b63dc109fa7960fdcc",
+    "url": "/Quiz-App/index.html"
+  },
+  {
+    "revision": "63d1981845e1f3b8ce9b",
+    "url": "/Quiz-App/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "e819485e4ab338fed58c",
+    "url": "/Quiz-App/static/js/2.c05d0765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz-App/static/js/2.c05d0765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d1981845e1f3b8ce9b",
+    "url": "/Quiz-App/static/js/main.d1901ef9.chunk.js"
+  },
+  {
+    "revision": "dc6ad8464665e2864c1d",
+    "url": "/Quiz-App/static/js/runtime-main.da63c0d7.js"
+  }
+]);
